aa0bdf414595f845a64a0224711b73fc469edd79
Samuel Van der Stappen <sam@Samuels-MacBook-Pro.local>
2022-09-23 14:04:11 +0200

    changed fzf commands and shell look

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ set fish_greeting ""
 
 set -gx TERM xterm-256color
 
+fzf_configure_bindings --directory=\cp
+
 #theme
 set -g theme_color_scheme terminal-dark
 set -g fish_prompt_pwd_dir_length 1
@@ -19,7 +21,12 @@ alias gs "git status"
 alias gl "git log"
 alias gd "git diff"
 alias gc "git commit -m"
-alias hb "hub browse"
+alias gaa "git add -A"
+alias gap "git add -p"
+alias gco "git checkout"
+alias gb "git branch"
+alias gcb "git checkout -b"
+alias hb "gh browse"
 alias c "open . -a \"Visual Studio Code\""
 
 alias config "/opt/homebrew/bin//git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
@@ -67,3 +74,48 @@ end
 set -gx PNPM_HOME "/Users/sam/Library/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
+
+  function fzf-rg-search-widget -d 'find text in files'
+
+    set rg_command "rg --ignore-case --files-with-matches -uu --iglob='!**/.git/' --follow"
+    # Start with all files; i.e. .*
+    FZF_DEFAULT_COMMAND="$rg_command '.*'" SHELL=fish fzf \
+      -m \
+      -e \
+      --ansi \
+      --disabled \
+      --bind "change:reload:$rg_command {q} || true" \
+      # I'm not sure what this `cut` does — but remove it and the whole thing fails.
+      --preview "rg -i --pretty --context 2 {q} {}" | cut -d":" -f1,2 \
+      | fzf_add_to_commandline
+  end
+
+
+  # The following few `__git` functions are Heavily adapted from
+  # https://gist.github.com/aluxian/9c6f97557b7971c32fdff2f2b1da8209
+  function __git_fzf_is_in_git_repo -d "Check whether we're in a git repo"
+    command -s -q git
+      and git rev-parse HEAD >/dev/null 2>&1
+  end
+
+  # Deciphered from fzf-file-widget. Surprising why it doesn't exist already!
+  function fzf_add_to_commandline -d 'add stdin to the command line, for fzf functions'
+    read -l -z result
+    commandline -t ""
+    # Do we need to escape this? This was here before, but doesn't work well with
+    # multiline strings.
+    # commandline -it -- (string escape $result)
+    commandline -it -- (echo $result | paste -s -d" " -)
+    commandline -f repaint
+  end
+
+# Emulates vim's cursor shape behavior
+# Set the normal and visual mode cursors to a block
+set fish_cursor_default block
+# Set the insert mode cursor to a line
+set fish_cursor_insert line
+# Set the replace mode cursor to an underscore
+set fish_cursor_replace_one underscore
+# The following variable can be used to configure cursor shape in
+# visual mode, but due to fish_cursor_default, is redundant here
+set fish_cursor_visual block
