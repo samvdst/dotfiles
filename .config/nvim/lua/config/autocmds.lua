3708fb78e6ddafc7ae8bbcff89dac7f27853c8be
Samuel Van der Stappen <sam@vd.st>
2024-10-18 16:03:22 +0200

    toggle remove trailing space

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,12 +2,33 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- Define a global variable to track the toggle state
+vim.g.remove_trailing_spaces = true
+
+-- Function to toggle the behavior
+function ToggleTrailingSpaces()
+  vim.g.remove_trailing_spaces = not vim.g.remove_trailing_spaces
+  if vim.g.remove_trailing_spaces then
+    print("Trailing spaces removal enabled")
+  else
+    print("Trailing spaces removal disabled")
+  end
+end
+
+-- Define the autogroup and autocmd
 local function augroup(name)
   return vim.api.nvim_create_augroup("samvdst_" .. name, { clear = true })
 end
 
-vim.api.nvim_create_autocmd({ "bufWritePre" }, {
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   group = augroup("removeTrailingSpaces"),
   pattern = "*",
-  command = [[%s/\s\+$//e]],
+  callback = function()
+    if vim.g.remove_trailing_spaces then
+      vim.cmd([[silent! %s/\s\+$//e]])
+    end
+  end,
 })
+
+-- Create a command to toggle the behavior
+vim.api.nvim_create_user_command("ToggleTrailingSpaces", ToggleTrailingSpaces, {})
--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -26,66 +26,66 @@ return {
     },
   },
 
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = {
-        auto_trigger = true,
-        keymap = {
-          accept = "<M-l>",
-          prev = "<M-[>",
-          next = "<M-]>",
-          dismiss = "<C-]>",
-        },
-      },
-      panel = {
-        auto_refresh = false,
-        keymap = {
-          accept = "<CR>",
-          jump_prev = "[[",
-          jump_next = "]]",
-          refresh = "gr",
-          open = "<M-CR>",
-        },
-      },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
-  },
-
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   opts = {
+  --     suggestion = {
+  --       auto_trigger = true,
+  --       keymap = {
+  --         accept = "<M-l>",
+  --         prev = "<M-[>",
+  --         next = "<M-]>",
+  --         dismiss = "<C-]>",
+  --       },
+  --     },
+  --     panel = {
+  --       auto_refresh = false,
+  --       keymap = {
+  --         accept = "<CR>",
+  --         jump_prev = "[[",
+  --         jump_next = "]]",
+  --         refresh = "gr",
+  --         open = "<M-CR>",
+  --       },
+  --     },
+  --     filetypes = {
+  --       markdown = true,
+  --       help = true,
+  --     },
+  --   },
+  -- },
+  --
   -- copilot cmp source
-  {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "zbirenbaum/copilot-cmp",
-        dependencies = "copilot.lua",
-        opts = {},
-        config = function(_, opts)
-          local copilot_cmp = require("copilot_cmp")
-          copilot_cmp.setup(opts)
-          -- attach cmp source whenever copilot attaches
-          -- fixes lazy-loading issues with the copilot cmp source
-          require("lazyvim.util").lsp.on_attach(function(client)
-            if client.name == "copilot" then
-              copilot_cmp._on_insert_enter({})
-            end
-          end)
-        end,
-      },
-    },
-    -- NOTE: I don't want copilot in my completions. I want ghost text for copilot and completions for intellisense
-    -- it appears that if we leave the above and uncomment the below, it works as expected. I'm not sure why we need the above tho.
-    --
-    -- ---@param opts cmp.ConfigSchema
-    -- opts = function(_, opts)
-    --   table.insert(opts.sources, 1, { name = "copilot", group_index = 2 })
-    --   opts.sorting = opts.sorting or require("cmp.config.default")().sorting
-    --   table.insert(opts.sorting.comparators, 1, require("copilot_cmp.comparators").prioritize)
-    -- end,
-  },
+  -- {
+  --   "nvim-cmp",
+  --   dependencies = {
+  --     {
+  --       "zbirenbaum/copilot-cmp",
+  --       dependencies = "copilot.lua",
+  --       opts = {},
+  --       config = function(_, opts)
+  --         local copilot_cmp = require("copilot_cmp")
+  --         copilot_cmp.setup(opts)
+  --         -- attach cmp source whenever copilot attaches
+  --         -- fixes lazy-loading issues with the copilot cmp source
+  --         require("lazyvim.util").lsp.on_attach(function(client)
+  --           if client.name == "copilot" then
+  --             copilot_cmp._on_insert_enter({})
+  --           end
+  --         end)
+  --       end,
+  --     },
+  --   },
+  --   -- NOTE: I don't want copilot in my completions. I want ghost text for copilot and completions for intellisense
+  --   -- it appears that if we leave the above and uncomment the below, it works as expected. I'm not sure why we need the above tho.
+  --   --
+  --   -- ---@param opts cmp.ConfigSchema
+  --   -- opts = function(_, opts)
+  --   --   table.insert(opts.sources, 1, { name = "copilot", group_index = 2 })
+  --   --   opts.sorting = opts.sorting or require("cmp.config.default")().sorting
+  --   --   table.insert(opts.sorting.comparators, 1, require("copilot_cmp.comparators").prioritize)
+  --   -- end,
+  -- },
 }
